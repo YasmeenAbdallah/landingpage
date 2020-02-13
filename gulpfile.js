812fd59745bc78bd9d2e7533275ae5c16a448bf6
ZulemaZahir <zulema11898@gmail.com>
2020-02-13 15:06:57 +0200

    modify the structure of file structure ,using sass & gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+// define gulp
+const gulp = require('gulp');
+
+// define sass 
+const sass = require('gulp-sass');
+
+// define htmlmin
+const htmlmin = require('gulp-htmlmin');
+
+//compile sass 
+sass.compiler = require('node-sass');
+
+// minify css 
+cssmin = require('gulp-cssnano'),
+
+    // sass function
+    gulp.task('sass', () => {
+        return gulp.src('./scss/*.scss')
+            .pipe(sass())
+            .pipe(cssmin())
+            .pipe(gulp.dest('./build'));
+    });
+
+    // html function
+    gulp.task('html', () => {
+        return gulp.src('./*.html')
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('./build'));
+    });
+
+    // sass watch file 
+    gulp.task('watch', () => {
+        gulp.watch('./scss/**/*.*', gulp.series('sass'));
+    });
+
+gulp.task('default', gulp.series('html', 'sass', 'watch'));
